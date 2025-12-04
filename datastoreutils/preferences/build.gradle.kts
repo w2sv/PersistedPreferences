f855d70ccadb209a795202b4d05a9474c5c37910
w2sv <zangenbergjanek@googlemail.com>
2025-12-04 21:08:14 +0100

    Update publish configuring & publish SourcesJar

--- datastoreutils/preferences/build.gradle.kts
+++ datastoreutils/preferences/build.gradle.kts
@@ -41,6 +41,11 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
 }
 
 publishing {
