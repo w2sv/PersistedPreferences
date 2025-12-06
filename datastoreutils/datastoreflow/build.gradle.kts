5dee877f4cea8c6a44b39e89228c308168b60c7f
w2sv <zangenbergjanek@googlemail.com>
2025-12-06 22:26:46 +0100

    Remove publication specifications that are unused by jitpack

--- datastoreutils/datastoreflow/build.gradle.kts
+++ datastoreutils/datastoreflow/build.gradle.kts
@@ -7,25 +7,8 @@ publishing {
         register<MavenPublication>("mavenJava") {
             groupId = "com.w2sv.datastoreutils"
             artifactId = "datastoreflow"
-            version = version.toString()
             afterEvaluate {
                 from(components["java"])
-            }
-            pom {
-                developers {
-                    developer {
-                        id.set("w2sv")
-                        name.set("Janek Zangenberg")
-                    }
-                }
-                description.set("Utilities for working with the androidx DataStore.")
-                url.set("https://github.com/w2sv/DataStoreUtils")
-                licenses {
-                    license {
-                        name.set("The Apache Software License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
             }
         }
     }
--- datastoreutils/preferences/build.gradle.kts
+++ datastoreutils/preferences/build.gradle.kts
@@ -7,25 +7,8 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.w2sv.datastoreutils"
             artifactId = "preferences"
-            version = version.toString()
             afterEvaluate {
                 from(components["release"])
-            }
-            pom {
-                developers {
-                    developer {
-                        id.set("w2sv")
-                        name.set("Janek Zangenberg")
-                    }
-                }
-                description.set("Utilities for working with the androidx DataStore.")
-                url.set("https://github.com/w2sv/DataStoreUtils")
-                licenses {
-                    license {
-                        name.set("The Apache Software License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
             }
         }
     }
